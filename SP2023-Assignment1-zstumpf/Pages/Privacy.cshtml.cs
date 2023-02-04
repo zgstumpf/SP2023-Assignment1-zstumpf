39c433c7c1a4c733a8da7e6e50660ff15b11f858
zacharystumpf <zacharystumpf@ZACHARYSTUM4516>
2023-02-04 15:11:26 -0600

    Add project files.

---
+++ SP2023-Assignment1-zstumpf/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SP2023_Assignment1_zstumpf.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
